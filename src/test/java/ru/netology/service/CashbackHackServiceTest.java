3924f57bfa9d50e5657261fbd17d3847a2d6bd5b
NetilogyIvan <inkornelyuk@gmail.com>
2023-05-21 21:27:47 +0600

    add tests CashbackHackService

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,69 @@
+package ru.netology.service;
+
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testForPurchaseOn1500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_500;
+        int expected = 500;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testForPurchaseOn1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testForPurchaseOn0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testForPurchaseOn1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testForPurchaseOn999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(actual, expected);
+
+    }
+
+    @org.junit.Test
+    public void testForPurchaseOn1001() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testForPurchaseOn2000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+}
